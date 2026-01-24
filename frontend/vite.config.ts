424be8db183d73ded9ecd3bd71c52fd04a183df9
ramong26 <rlatndus21049@gmail.com>
2026-01-24 22:38:23 +0900

    feat: deploy - 깃허브 페이지 배포

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/TAT",
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
